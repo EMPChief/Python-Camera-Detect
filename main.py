7dff2c3c24d1bc7aeaaa2756e965321e81e4be89
EMPChief <bjornmagnekristensen1999@gmail.com>
2024-02-20 10:18:47 +0100

    Improved naming, made motion sensor

--- main.py
+++ main.py
@@ -2,26 +2,38 @@ import cv2
 import time
 from EmailSender import EmailSender
 
-
 VIDEO_DEVICE_INDEX = 0
-# VIDEO_DEVICE_INDEX = int(input("Enter video device index: (0(webcam), 1(external camera), 2(external camera)): "))
 video_capture = cv2.VideoCapture(VIDEO_DEVICE_INDEX)
 time.sleep(3)
-first_frame = None
+previous_frame = None
+
 while True:
     success, frame = video_capture.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blurred_gray_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0)
-    if first_frame is None:
-        first_frame = blurred_gray_frame
+
+    if previous_frame is None:
+        previous_frame = blurred_gray_frame
         continue
-    
-    cv2.imshow("Video", blurred_gray_frame)
-    
+
+    delta_frame = cv2.absdiff(previous_frame, blurred_gray_frame)
+    threshold_frame = cv2.threshold(delta_frame, 51, 255, cv2.THRESH_BINARY)[1]
+    dilated_frame = cv2.dilate(threshold_frame, None, iterations=3)
+    contours, check = cv2.findContours(dilated_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 10000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    cv2.imshow("Motion Detection", frame)
+
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
 
 video_capture.release()
 cv2.destroyAllWindows()
-EmailSender().send_email("Test Subject", "Test Body", "empchief@gmail.com")
+
+EmailSender().send_email("Motion Detected", "Motion was detected in the video stream.", "support@empchief.com")

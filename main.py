b4fb5a2f77fff5df9d1b53cfcc04891ed2852881
EMPChief <bjornmagnekristensen1999@gmail.com>
2024-02-20 09:58:52 +0100

    Add video device index selection and email sending functionality

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import cv2
 import time
+from EmailSender import EmailSender
 
-# Video capture device selection:
-# 0: webcam
-# 1: external camera
-# 2: external camera
-VIDEO_DEVICE = 0
-video_capture = cv2.VideoCapture(VIDEO_DEVICE)
+
+VIDEO_DEVICE_INDEX = 0
+# VIDEO_DEVICE_INDEX = int(input("Enter video device index: (0(webcam), 1(external camera), 2(external camera)): "))
+video_capture = cv2.VideoCapture(VIDEO_DEVICE_INDEX)
 time.sleep(3)
-
+first_frame = None
 while True:
     success, frame = video_capture.read()
-    print(f"Success: {success}")
-    print(f"Frame: {frame}")
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred_gray_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+    if first_frame is None:
+        first_frame = blurred_gray_frame
+        continue
+    
+    cv2.imshow("Video", blurred_gray_frame)
+    
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
 
 video_capture.release()
 cv2.destroyAllWindows()
+EmailSender().send_email("Test Subject", "Test Body", "empchief@gmail.com")
